0314c087f3c24e26dd47752a73401d7f44132abe
Martin Cervantes <cervantes.martine@gmail.com>
2021-07-22 13:03:53 -0600

    Add heroes to the component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
+import { Hero, HEROES } from './hero';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +8,14 @@ import { Hero } from './hero';
 })
 
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
